56d9f6ec823e9da9df1e8280c2c92d715c29449d
Nicholas Mutale <nicomutale@gmail.com>
2022-07-11 12:38:56 +0200

    Generate categories controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+  get 'categories/new'
   get 'pages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
