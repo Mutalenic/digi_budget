5a7090aedea7f673846f3e0e9ec4e55a5e0fae13
Nicholas Mutale <nicomutale@gmail.com>
2022-07-11 14:55:53 +0200

    Add validations to payment model

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,8 @@
 class Payment < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :amount, presence: true, numericality: { greater_than: 0, less_than: 1000000 }
+  validates :user_id, presence: true
 end
