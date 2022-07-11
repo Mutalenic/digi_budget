61ea6a4333d24132ff95a16cdc402944c3727ca9
Nicholas Mutale <nicomutale@gmail.com>
2022-07-11 20:01:39 +0200

    Update categories index

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :user
-  has_many :payments, dependent: :destroy_all
+  has_many :payments, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :icon, presence: true
