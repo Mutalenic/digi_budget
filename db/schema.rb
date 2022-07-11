aedbdcccdc1c90a6ac58c8502f0b7618fbdb0fea
Nicholas Mutale <nicomutale@gmail.com>
2022-07-11 13:21:08 +0200

    Run payments migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_11_105226) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_11_105751) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_11_105226) do
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
+  create_table "payments", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount"
+    t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_payments_on_category_id"
+    t.index ["user_id"], name: "index_payments_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -30,4 +41,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_11_105226) do
   end
 
   add_foreign_key "categories", "users"
+  add_foreign_key "payments", "categories"
+  add_foreign_key "payments", "users"
 end
