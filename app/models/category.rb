346242b14e16a35dd266e5d9800908f537b956cd
Nicholas Mutale <nicomutale@gmail.com>
2022-07-11 14:53:45 +0200

    Add validations to category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,11 @@
 class Category < ApplicationRecord
   belongs_to :user
+  has_many :payments, dependent: :destroy_all
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :icon, presence: true
+
+  def recent_transactio
+    payments.order(created_at: :desc).limit(5)
+  end
 end
