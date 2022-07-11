39d4a2ccac5e2ba3b13672e090ce50c6a89e7b78
Nicholas Mutale <nicomutale@gmail.com>
2022-07-11 18:29:47 +0200

    Update pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+  before_action :set_user, only: [:show, :update, :destroy]
+
   def index
+    @users = User.all
   end
 end
